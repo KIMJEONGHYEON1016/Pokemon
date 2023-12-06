e1dd3c679de8b7d380ccda5070ae4a04d455364e
김정현 <dkemfdk4@naver.com>
2023-12-06 19:34:14 +0900

    Feat:main_poke_api

--- PokemonApp/Services/PokeService.swift
+++ PokemonApp/Services/PokeService.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class PokeService {
+    func getData(url: String, completion: @escaping (PokemonSprites?) ->()) {
+        guard let url = URL(string: url) else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, reponse, error) in
+            guard let data = data else { return }
+            
+            do {
+                let pokemonSprite = try JSONDecoder().decode(PokemonSelected.self, from: data)
+                DispatchQueue.main.async {
+                    completion(pokemonSprite.sprites)
+                }
+            } catch {
+                print("Error decoding JSON:", error)
+                DispatchQueue.main.async {
+                    completion(nil) // JSON 디코딩 실패 시 nil을 반환하거나 적절한 오류 처리
+                }
+            }
+        }.resume()
+    }
+}
+
+
--- PokemonApp/View/MainView.swift
+++ PokemonApp/View/MainView.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class MainView: UIViewController {
+    class MainView: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        @IBOutlet weak var mainPokemon: UIImageView!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            PokeService().getData(url: "https://pokeapi.co/api/v2/pokemon/25/") { [weak self] pokemonSprites in
+                       if let urlString = pokemonSprites?.front_default, let imageURL = URL(string: urlString) {
+                           // Download the image asynchronously
+                           DispatchQueue.global().async {
+                               if let imageData = try? Data(contentsOf: imageURL) {
+                                   // Update UI on the main queue
+                                   DispatchQueue.main.async {
+                                       self?.mainPokemon.image = UIImage(data: imageData)
+                                   }
+                               }
+                           }
+                       }
+                   }
+               }
+
+
     }
-
-
-}
 
