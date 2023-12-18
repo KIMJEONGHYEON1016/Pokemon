8cdcebac77f2a151d1f96afaa655b7677527a504
김정현 <dkemfdk4@naver.com>
2023-12-18 17:21:04 +0900

    Feat: mypokecolletion_ui

---
+++ PokemonApp/View/MyPokemonColletionView.swift
@@ -0,0 +1,16 @@
+//
+//  MyPokemonList.swift
+//  PokemonApp
+//
+//  Created by 김정현 on 2023/12/18.
+//
+
+import Foundation
+
+class MyPokemonColletionView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
